332e628bc2aedc0c63ce950d7530a8336bb01155
FishFinsMertz <thn.anbui@gmail.com>
2024-08-17 15:09:45 -0400

    Added some prefab stuff to spawn enemies

--- GMTKGameJam2024/Assets/Scripts/EnemyScripts/EnemyLineUp.cs
+++ GMTKGameJam2024/Assets/Scripts/EnemyScripts/EnemyLineUp.cs
@@ -6,11 +6,13 @@ public class EnemyLineUp : MonoBehaviour
 {   
     //Variables
     public BaseEnemy[] enemyLineUp;
+    public GameObject enemyPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+         CreateLineUp(); //For testing purposes
+         Debug.Log("Line up called");
     }
 
     // Update is called once per frame
@@ -19,8 +21,34 @@ public class EnemyLineUp : MonoBehaviour
         
     }
 
-    public void CreateLineUp() {
-        //TODO: Generates a lineup for the enemies
+        public void CreateLineUp()
+    {
+        int numberOfEnemies = 3;
+        enemyLineUp = new BaseEnemy[numberOfEnemies];
+
+        Debug.Log("Creating enemy lineup...");
+
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            // Instantiate the enemy from the prefab
+            GameObject enemyObject = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+
+            Debug.Log("Enemy " + i + " instantiated.");
+
+            // Get the BaseEnemy component from the instantiated object
+            BaseEnemy enemy = enemyObject.GetComponent<BaseEnemy>();
+
+            // Assign the enemy to the array
+            enemyLineUp[i] = enemy;
+
+            // Optionally, set attack and health stats
+            enemy.attack = Random.Range(10, 20); // Example values
+            enemy.health = Random.Range(50, 100); // Example values
+
+            Debug.Log("Enemy " + i + " - Attack: " + enemy.attack + ", Health: " + enemy.health);
+        }
+
+        Debug.Log("Enemy lineup creation complete. Total enemies: " + enemyLineUp.Length);
     }
 
     public void BeginAttack() {
