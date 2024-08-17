992e2139254e85b68b101e8ef6e499f90918bb86
FishFinsMertz <thn.anbui@gmail.com>
2024-08-17 11:43:21 -0400

    Updated EnemyLineUp.cs

--- GMTKGameJam2024/Assets/Scripts/EnemyScripts/EnemyLineUp.cs
+++ GMTKGameJam2024/Assets/Scripts/EnemyScripts/EnemyLineUp.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyLineUp : MonoBehaviour
-{
+{   
+    //Variables
+    public BaseEnemy[] enemyLineUp;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,13 @@ public class EnemyLineUp : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CreateLineUp() {
+        //TODO: Generates a lineup for the enemies
+    }
+
+    public void BeginAttack() {
+        //TODO: Iterate through enemy array and have each enemy attack 
     }
 }
