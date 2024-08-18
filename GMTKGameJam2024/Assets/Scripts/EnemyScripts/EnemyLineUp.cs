74d6e64b453dd55ecfab8bd553019719f806bc0e
FishFinsMertz <thn.anbui@gmail.com>
2024-08-17 20:45:28 -0400

    Minor changes

--- GMTKGameJam2024/Assets/Scripts/EnemyScripts/EnemyLineUp.cs
+++ GMTKGameJam2024/Assets/Scripts/EnemyScripts/EnemyLineUp.cs
@@ -14,11 +14,11 @@ public class EnemyLineUp : MonoBehaviour
     {   
         //For testing purposes
         Debug.Log("EnemyLineUp: Line up created");
-        this.CreateLineUp();
+        this.CreateLineUp(numberOfEnemies);
         Debug.Log("EnemyLineUp: Attack called");
         this.BeginAttack();
-        Debug.Log("EnemyLineUp: apply damage called");
-        this.ApplyDamageToAll(100);
+        //Debug.Log("EnemyLineUp: apply damage called");
+        //this.ApplyDamageToAll(100);
     }
 
     // Update is called once per frame
@@ -27,7 +27,7 @@ public class EnemyLineUp : MonoBehaviour
         
     }
 
-    public void CreateLineUp(){
+    public void CreateLineUp(int numberOfEnemies){
         enemyLineUp = new Enemy[numberOfEnemies];
 
         Debug.Log("EnemyLineUp.cs: Creating enemy lineup...");
@@ -43,17 +43,12 @@ public class EnemyLineUp : MonoBehaviour
             // Instantiate the enemy at the random position
             GameObject enemyObject = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
 
-            //Debug.Log("Enemy " + i + " instantiated at position: " + randomPosition);
-
             // Get the Enemy component from the instantiated object
             Enemy enemy = enemyObject.GetComponent<Enemy>();
 
             // Assign the enemy to the array and the stats n stuff
             enemyLineUp[i] = enemy;
-            enemy.attack = Random.Range(10, 20); // Example values
             enemy.health = Random.Range(50, 100); // Example values
-
-            //Debug.Log("Enemy " + i + " - Attack: " + enemy.attack + ", Health: " + enemy.health);
         }
 
         Debug.Log("EnemyLineUp.cs: Enemy lineup creation complete. Total enemies: " + enemyLineUp.Length);
@@ -63,7 +58,6 @@ public class EnemyLineUp : MonoBehaviour
     public void BeginAttack() {
         //TODO: Iterate through enemy array and have each enemy attack 
         for (int i = 0; i < numberOfEnemies; i++) {
-            //Debug.Log(enemyLineUp[i].attack + ", " + enemyLineUp[i].health);
             enemyLineUp[i].Attack();
         }
         Debug.Log("EnemyLineUp.cs: Attack completed");
