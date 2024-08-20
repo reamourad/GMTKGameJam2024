50466eb901c4ba80da67de38154cde6d1ca5fb42
MrAwesomeJr <58062648+MrAwesomeJr@users.noreply.github.com>
2024-08-20 11:11:32 -0400

    changes for webgl

--- GMTKGameJam2024/Assets/Scripts/TetrisGrid/TetrisGrid.cs
+++ GMTKGameJam2024/Assets/Scripts/TetrisGrid/TetrisGrid.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.WSA;
+// using UnityEngine.WSA;
 
 public class TetrisGrid : MonoBehaviour
 {
