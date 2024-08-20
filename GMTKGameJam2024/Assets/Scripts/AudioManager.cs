1f74f7ab890ec6264c188412bb80c43ce728fd58
FishFinsMertz <thn.anbui@gmail.com>
2024-08-20 10:26:19 -0400

    Some audio stuff

---
+++ GMTKGameJam2024/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{   
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+    public AudioClip blockPlaced;
+    public AudioClip playerAttack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
