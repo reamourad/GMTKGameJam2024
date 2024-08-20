49ea8ceb22cb3c4c7c70d3396350b6ad083edbf6
FishFinsMertz <thn.anbui@gmail.com>
2024-08-20 10:31:44 -0400

    Updated Audio

--- GMTKGameJam2024/Assets/Scripts/AudioManager.cs
+++ GMTKGameJam2024/Assets/Scripts/AudioManager.cs
@@ -14,12 +14,18 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicSource.clip = background;
+        musicSource.loop = true;
+        musicSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlaySFX(AudioClip clip) {
+        SFXSource.PlayOneShot(clip);
     }
 }
